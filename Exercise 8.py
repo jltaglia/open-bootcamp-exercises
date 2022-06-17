89264f4d02a605e98851958196a3e66ccd6913a0
Julian Tagliarini <jltaglia2gmail.com>
2022-06-17 16:48:02 -0300

    Rev

--- Exercise 8.py
+++ Exercise 8.py
@@ -1,6 +1,10 @@
 inicio = int(input("Ingrese el valor inicial: "))
 fin = int(input("Ingrese el valor final: "))
 
+print(type(inicio))
+print(type(fin))
+
+
 impares = []
 for i in range(inicio, fin+1):
     if i % 2 != 0:
