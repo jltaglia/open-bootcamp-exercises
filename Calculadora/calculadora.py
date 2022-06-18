0a31686dc7112ae06f0689e803ce275dc452972a
Julian Tagliarini <jltaglia2gmail.com>
2022-06-18 13:36:57 -0300

    Rev

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,20 @@
+'''
+Este módulo contiene las operaciones básicas de una calculadora:
+sumar, restar, multiplicar y dividir.
+
+Este módulo lo importaréis a un archivo python y 
+llamaréis a las funciones creadas. 
+Los resultados tenéis que mostrarlos por consola.
+'''
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
---
+++ Calculadora/main.py
@@ -0,0 +1,15 @@
+import calculadora as calc
+
+s = calc.sumar(10,15)
+
+r = calc.restar(10,15)
+
+m = calc.multiplicar(10,15)
+
+d = calc.dividir(10,15)
+
+print('suma =', s)
+print('resta =', r)
+print('multiplicación =', m)
+print('división =', d)
+
